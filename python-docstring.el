61e9fdc1a950dfc8769b831ee04390db6c36f43a
Laurens Van Houtven <_@lvh.io>
2015-04-20 13:56:42 -0700

    Add some stuff up top

--- python-docstring.el
+++ python-docstring.el
@@ -1,3 +1,35 @@
+;;; python-docstring-mode.el --- Smart Python docstring formatting
+
+;; Copyright (c) 2014 The Authors
+;;
+;; Permission is hereby granted, free of charge, to any person obtaining
+;; a copy of this software and associated documentation files (the
+;; "Software"), to deal in the Software without restriction, including
+;; without limitation the rights to use, copy, modify, merge, publish,
+;; distribute, sublicense, and/or sell copies of the Software, and to
+;; permit persons to whom the Software is furnished to do so, subject to
+;; the following conditions:
+;;
+;; The above copyright notice and this permission notice shall be
+;; included in all copies or substantial portions of the Software.
+;;
+;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+;; EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+;; MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+;; NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+;; LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+;; OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+;; WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+;;; Commentary:
+
+;; python-docstring-mode.el is a minor mode for intelligently
+;; reformatting (refilling) and highlighting Python docstrings. It
+;; understands both epytext and Sphinx formats (even intermingled!),
+;; so it knows how to reflow them correctly. It will also highlight
+;; markup in your docstrings, including epytext and reStructuredText.
+
+;;; Code:
 
 (defvar python-docstring-script
   (concat (file-name-as-directory (file-name-directory #$))
@@ -129,3 +161,5 @@ Null prefix argument turns off the mode."
   )
 
 (provide 'python-docstring)
+
+;;; python-docstring.el ends here
